5d81a52142c99188f766e1fabee791f27988dd84
theguiltyperson <1023635049@qq.com>
2020-03-11 22:50:26 +0800

    Update ProvinceDAO.java
    接口

--- src/infectstatistic/dao/ProvinceDAO.java
+++ src/infectstatistic/dao/ProvinceDAO.java
@@ -1,5 +1,13 @@
 package infectstatistic.dao;
 
+import infectstatistic.pojo.Province;
+
+import java.util.List;
+
 public interface ProvinceDAO {
+    Province get(String name, String date);//通过省份名和日期获取省份数据
+    List<Province> list(String date);//返回指定日期的所有省份数据
+    List<Province> list(String name,boolean b);//通过省份名获取省份数据
+    List<Province> list();//列出所有省份数据
 
 }
