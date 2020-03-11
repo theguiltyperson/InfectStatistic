a868577e3928f79486328d20762556c587210299
theguiltyperson <1023635049@qq.com>
2020-03-11 20:51:22 +0800

    Update Province.java

--- src/infectstatistic/pojo/Province.java
+++ src/infectstatistic/pojo/Province.java
@@ -2,6 +2,7 @@ package infectstatistic.pojo;
 
 public class Province {
     private String name;//省份名
+    private String date;//日期
     private int ip;//感染
     private int sp;//疑似
     private int cure;//治愈
@@ -13,6 +14,14 @@ public class Province {
 
     public String getName() {
         return name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
     }
 
     public void setIp(int ip) {
